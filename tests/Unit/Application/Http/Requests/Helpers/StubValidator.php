80abee9b948d54c6ce016136adfba552d00df3cb
abouhassane <abdu.abou@gmail.com>
2023-11-25 23:17:23 +0100

    Add a request test case to allow unit tests on request validators

---
+++ tests/Unit/Application/Http/Requests/Helpers/StubValidator.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Application\Http\Requests\Helpers;
+
+use Illuminate\Validation\Validator;
+
+use function array_slice;
+use function is_array;
+
+final class StubValidator extends Validator
+{
+    /**
+     * @param array<string, mixed> $data
+     * @param array<string, string> $rules
+     */
+    public function __construct(array $data, array $rules)
+    {
+        parent::__construct(new StubTranslator(), $data, $rules);
+    }
+
+    /**
+     * @param string $attribute
+     * @param mixed $value
+     * @param array<int, int|string> $parameters
+     */
+    public function validateExists($attribute, $value, $parameters): bool
+    {
+        $columnId = $value;
+        $tableName = $parameters[0] ?? '';
+        $columnName = $parameters[1] ?? '';
+        $extraParameters = array_slice($parameters, 2);
+
+        if ($attribute !== 'id' && ($columnName !== '' && $columnName !== 'id')) {
+            return StubRepository::has((string) $tableName, (string) $columnName, $value);
+        }
+
+        if (is_array($columnId)) {
+            return StubRepository::allExist((string) $tableName, $columnId, $extraParameters);
+        }
+
+        return StubRepository::exists((string) $tableName, $columnId, $extraParameters);
+    }
+
+    /**
+     * @param string $attribute
+     * @param string $value
+     * @param array<int, int|string> $parameters
+     */
+    public function validateUnique($attribute, $value, $parameters): bool
+    {
+        $tableName = $parameters[0] ?? '';
+        $columnName = $parameters[1] ?? '';
+
+        return ! (StubRepository::has((string) $tableName, (string) $columnName, $value));
+    }
+}
---
+++ tests/Unit/Application/Http/Requests/RequestTestCase.php
@@ -0,0 +1,99 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Application\Http\Requests;
+
+use Exception;
+use Illuminate\Container\Container;
+use Illuminate\Contracts\Validation\Factory as ValidationFactory;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Redirector;
+use Illuminate\Routing\Route;
+use Illuminate\Routing\RouteCollection;
+use Illuminate\Routing\UrlGenerator;
+use Tests\Unit\Application\Http\Requests\Helpers\StubRepository;
+use Tests\Unit\Application\Http\Requests\Helpers\StubValidationFactory;
+use Tests\Unit\UnitTestCase;
+
+abstract class RequestTestCase extends UnitTestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+        StubRepository::clearRepository();
+    }
+
+    /**
+     * @param array<string, mixed> $requestParameters
+     */
+    public function validateParameters(array $requestParameters): bool
+    {
+        try {
+            $this->createRequest($requestParameters)->validateResolved();
+        } catch (Exception) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * @param array<string, mixed> $requestParameters
+     * @param array<string, mixed> $routeParameters
+     */
+    public function validateWithRouteParameters(array $requestParameters, array $routeParameters): bool
+    {
+        try {
+            $formRequest = $this->createRequest($requestParameters);
+            $this->setRequestRouteParameters($formRequest, $routeParameters);
+            $formRequest->validateResolved();
+        } catch (Exception) {
+            return false;
+        }
+
+        return true;
+    }
+
+    abstract protected function getRequestUnderTest(): string;
+
+    private function createContainer(): Container
+    {
+        $container = new Container();
+        $container->bindMethod($this->getRequestUnderTest() . '@authorize', static fn (): bool => true);
+        $container->bind(ValidationFactory::class, StubValidationFactory::class);
+
+        return $container;
+    }
+
+    /**
+     * @param array<string, mixed> $requestParameters
+     */
+    private function createRequest(array $requestParameters): FormRequest
+    {
+        $requestUnderTestClass = $this->getRequestUnderTest();
+        /** @var FormRequest $request */
+        $request = new $requestUnderTestClass($requestParameters);
+        $request->setContainer($this->createContainer());
+        $request->setRedirector(new Redirector(new UrlGenerator(new RouteCollection(), new Request())));
+
+        return $request;
+    }
+
+    /**
+     * @param array<string, mixed> $routeParameters
+     */
+    private function setRequestRouteParameters(FormRequest $formRequest, array $routeParameters): void
+    {
+        $formRequest->setRouteResolver(static function () use ($formRequest, $routeParameters): Route {
+            $route = new Route('', '', []);
+            $route->bind($formRequest);
+            foreach ($routeParameters as $parameterName => $parameterValue) {
+                $route->setParameter($parameterName, $parameterValue);
+            }
+
+            return $route;
+        });
+    }
+}
