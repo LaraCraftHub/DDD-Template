e656526068d4675b06362994fd4bd5d7e95b4b2c
Abouhassane Abdelhamid <abdu.abou@gmail.com>
2023-11-25 14:53:35 +0100

    Add phpstan code analyser (#8)
    Add PHPStan code analyser with some pre-configured rules:
    - Phpstan-deprecation-rules
    - Larastan
    - Phpstan-mockery
    - Phpstan-phpunit

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -13,6 +13,7 @@ class ExampleTest extends TestCase
      */
     public function test_that_true_is_true(): void
     {
+        // @phpstan-ignore-next-line
         $this->assertTrue(true);
     }
 }
