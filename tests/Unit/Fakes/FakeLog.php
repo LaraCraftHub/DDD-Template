0fecacaf107791c3e9b057d64acd5c206ad5dbf7
Abouhassane Abdelhamid <abdu.abou@gmail.com>
2023-11-25 23:14:46 +0100

    Add a unit test case (#16)
    A UnitTestCase that does not bootstrap the whole Laravel application

---
+++ tests/Unit/Fakes/FakeLog.php
@@ -0,0 +1,122 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Fakes;
+
+use Psr\Log\LoggerInterface;
+
+class FakeLog implements LoggerInterface
+{
+    /** @var array<string, array<array<string, mixed>>> */
+    private array $log = [
+        'emergency' => [],
+        'alert' => [],
+        'critical' => [],
+        'error' => [],
+        'warning' => [],
+        'notice' => [],
+        'info' => [],
+        'debug' => [],
+        'log' => [],
+    ];
+
+    public function flush(string $level): void
+    {
+        $this->log[$level] = [];
+    }
+
+    /**
+     * @return array<array<string, mixed>>
+     */
+    public function get(string $level): array
+    {
+        return $this->log[$level];
+    }
+
+    /**
+     * @param string $message
+     */
+    public function emergency($message, array $context = []): void
+    {
+        $this->log('emergency', $message, $context);
+    }
+
+    /**
+     * @param string $message
+     */
+    public function alert($message, array $context = []): void
+    {
+        $this->log('alert', $message, $context);
+    }
+
+    /**
+     * @param string $message
+     */
+    public function critical($message, array $context = []): void
+    {
+        $this->log('critical', $message, $context);
+    }
+
+    /**
+     * @param string $message
+     */
+    public function error($message, array $context = []): void
+    {
+        $this->log('error', $message, $context);
+    }
+
+    /**
+     * @param string $message
+     */
+    public function warning($message, array $context = []): void
+    {
+        $this->log('warning', $message, $context);
+    }
+
+    /**
+     * @param string $message
+     */
+    public function notice($message, array $context = []): void
+    {
+        $this->log('notice', $message, $context);
+    }
+
+    /**
+     * @param string $message
+     */
+    public function info($message, array $context = []): void
+    {
+        $this->log('info', $message, $context);
+    }
+
+    /**
+     * @param string $message
+     */
+    public function debug($message, array $context = []): void
+    {
+        $this->log('debug', $message, $context);
+    }
+
+    /**
+     * @param string $level
+     * @param string $message
+     */
+    public function log($level, $message, array $context = []): void
+    {
+        $this->log[$level][] = [
+            'message' => $message,
+            'context' => $context,
+        ];
+    }
+
+    public static function channel(string $level): self
+    {
+        return new self();
+    }
+
+    public static function getLogger(): self
+    {
+        return new self();
+    }
+}
