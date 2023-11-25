96206aa66fbed9b8909dc7e301766c1224edd98f
Abouhassane Abdelhamid <abdu.abou@gmail.com>
2023-11-25 21:16:06 +0100

    Feat: Add infrastructure layer folder (#13)
    * Split pre-occupation of User entity into User Domain and EloquentUser Infrastructure
    
    * Add implementation for UserRepository
    
    * Explicit where PHPStan should run
    
    * TODO: Activate LaravelSetList::LARAVEL_STATIC_TO_INJECTION only for infrastructure layer

--- app/Domain/User/User.php
+++ app/Domain/User/User.php
@@ -5,18 +5,19 @@ declare(strict_types=1);
 namespace App\Domain\User;
 
 use App\Domain\BusinessEntity;
+use App\Infrastructure\User\EloquentUser;
 use Carbon\CarbonImmutable;
-use Database\Factories\UserFactory;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Auth\MustVerifyEmail;
+use Illuminate\Auth\Passwords\CanResetPassword;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+use Illuminate\Contracts\Auth\MustVerifyEmail as MustVerifyEmailContract;
 use Illuminate\Database\Eloquent\Collection;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\SoftDeletes;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Foundation\Auth\Access\Authorizable;
 use Illuminate\Notifications\DatabaseNotification;
 use Illuminate\Notifications\DatabaseNotificationCollection;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 use Laravel\Sanctum\PersonalAccessToken;
 
 /**
@@ -38,48 +39,12 @@ use Laravel\Sanctum\PersonalAccessToken;
  * @property-read Collection<int, PersonalAccessToken> $tokens
  * @property-read int|null $tokens_count
  *
- * @method static UserFactory factory($count = null, $state = [])
- * @method static Builder|User newModelQuery()
- * @method static Builder|User newQuery()
- * @method static Builder|User query()
- *
  * @mixin \Eloquent
  */
-class User extends Authenticatable implements BusinessEntity, MustVerifyEmail
+class User extends EloquentUser implements AuthenticatableContract, AuthorizableContract, BusinessEntity, CanResetPasswordContract, MustVerifyEmailContract
 {
-    use HasApiTokens;
-    use HasFactory;
-    use Notifiable;
-    use SoftDeletes;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'immutable_datetime',
-        'password' => 'hashed',
-    ];
+    use Authenticatable;
+    use Authorizable;
+    use CanResetPassword;
+    use MustVerifyEmail;
 }
---
+++ app/Infrastructure/User/Repositories/EloquentUserRepository.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure\User\Repositories;
+
+use App\Domain\User\Repositories\UserRepository;
+
+final class EloquentUserRepository implements UserRepository
+{
+}
