1783ba7ef67d976cba7e362e4045d209132d005e
abouhassane <abdu.abou@gmail.com>
2023-11-25 23:05:59 +0100

    Add StubModelOperation serving as Mock for base model operations

---
+++ tests/Builder/Stub/StubModelOperation.php
@@ -0,0 +1,120 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Builder\Stub;
+
+use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Collection;
+use Mockery;
+
+use function array_key_exists;
+
+/**
+ * @template StubModel
+ */
+trait StubModelOperation
+{
+    /**
+     * @param array<string, bool> $options
+     */
+    public function save(array $options = []): bool
+    {
+        return true;
+    }
+
+    /**
+     * @param array<string, mixed> $attributes
+     * @param array<string, bool> $options
+     */
+    public function update(array $attributes = [], array $options = []): bool
+    {
+        if (! array_key_exists('updated_at', $attributes)) {
+            $this->setAttribute('updated_at', now()->addSeconds(5));
+        }
+
+        foreach ($attributes as $attribute => $value) {
+            $this->setAttribute($attribute, $value);
+        }
+
+        return true;
+    }
+
+    /**
+     * @param array<array-key, (callable(EloquentBuilder<StubModel>): mixed)|string>|string $relations
+     */
+    public function load($relations): self
+    {
+        return $this;
+    }
+
+    /**
+     * @param array<array-key, (callable(EloquentBuilder<StubModel>): mixed)|string>|string $relations
+     */
+    public function loadMissing($relations): self
+    {
+        return $this;
+    }
+
+    /**
+     * @param array<array-key, string>|string $with
+     */
+    public function fresh($with = []): self
+    {
+        return $this;
+    }
+
+    public function refresh(): self
+    {
+        return $this;
+    }
+
+    public function delete(): bool
+    {
+        $this->attributes['deleted_at'] = now();
+
+        return true;
+    }
+
+    /**
+     * @param Collection<int, StubModel> $expectedValue
+     * @return HasMany<StubModel>
+     */
+    public function mockHasMany(Collection $expectedValue): HasMany
+    {
+        $eloquentBuilder = Mockery::spy(EloquentBuilder::class);
+
+        $eloquentBuilder->shouldReceive('getModel')->andReturn($this);
+        $eloquentBuilder->shouldReceive('where')->andReturn($eloquentBuilder);
+        $eloquentBuilder->shouldReceive('WhereNotNull')->andReturn($eloquentBuilder);
+
+        $eloquentBuilder->shouldReceive('get')->andReturn($expectedValue);
+        $eloquentBuilder->shouldReceive('first')->andReturn($expectedValue->first());
+
+        return $this->newHasMany($eloquentBuilder, $this, '', '');
+    }
+
+    /**
+     * @param Collection<int, StubModel> $expectedValue
+     * @return BelongsToMany<StubModel>
+     */
+    public function mockBelongsToMany(Collection $expectedValue): BelongsToMany
+    {
+        $eloquentBuilder = Mockery::spy(EloquentBuilder::class);
+
+        $eloquentBuilder->shouldReceive('getModel')->andReturn($this);
+        $eloquentBuilder->shouldReceive('applyScopes')->andReturn($eloquentBuilder);
+        $eloquentBuilder->shouldReceive('getQuery')->andReturn($this);
+        $eloquentBuilder->shouldReceive('addSelect')->andReturn($eloquentBuilder);
+        $eloquentBuilder->shouldReceive('getModels')->andReturn($expectedValue->all());
+        $eloquentBuilder->shouldReceive('eagerLoadRelations')->andReturn($expectedValue->all());
+
+        $eloquentBuilder->shouldReceive('join')->andReturn();
+        $eloquentBuilder->shouldReceive('take')->andReturn($eloquentBuilder);
+        $eloquentBuilder->shouldReceive('where')->andReturn($eloquentBuilder);
+
+        return $this->newBelongsToMany($eloquentBuilder, $this, '', '', '', '', '');
+    }
+}
