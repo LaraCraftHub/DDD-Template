f68b5097a8599dfff79533510a9eb5cd6c3ed9be
abouhassane <abdu.abou@gmail.com>
2023-11-25 13:16:35 +0100

    Register ide-helper service provider in local env

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +12,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        if (! $this->app->isLocal()) {
+            return;
+        }
+
+        $this->app->register(IdeHelperServiceProvider::class);
     }
 
     /**
