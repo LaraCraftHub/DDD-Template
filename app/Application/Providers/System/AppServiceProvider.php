f9dbb73ff5b9ddf9b65adb17042a553002f71be0
abouhassane <abdu.abou@gmail.com>
2023-11-25 19:53:22 +0100

    Use CarbonImmutable instance for models dates properties

--- app/Application/Providers/System/AppServiceProvider.php
+++ app/Application/Providers/System/AppServiceProvider.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace App\Application\Providers\System;
 
 use Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider;
+use Carbon\CarbonImmutable;
+use Illuminate\Support\Facades\Date;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -26,6 +28,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        /**
+         * Whenever you use now() helper or the Date facade in your code, you'll always get a CarbonImmutable instance.
+         * Likewise, if you were to use the freshTimestamp() method on a model, or have any cast values,
+         * these will always be returned as an instance of CarbonImmutable.
+         * Be aware Carbon\Carbon or Illuminate\Support\Carbon will still return a mutable instance of carbon,
+         * instead use Illuminate\Support\Facades\Date;
+         */
+        Date::use(CarbonImmutable::class);
     }
 }
